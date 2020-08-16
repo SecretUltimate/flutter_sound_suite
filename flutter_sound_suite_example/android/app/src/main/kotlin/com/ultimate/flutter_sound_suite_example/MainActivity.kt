50b4cb7bfef6ec040aefd9bdcab7159e8a1c7a2e
Cloud <chanyunyu@vip.qq.com>
2020-08-16 22:18:27 +0800

    add example for recorder and player

---
+++ flutter_sound_suite_example/android/app/src/main/kotlin/com/ultimate/flutter_sound_suite_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ultimate.flutter_sound_suite_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ flutter_sound_suite_example/lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sound_suite/flutter_sound_suite.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(FlutterSoundSuiteApp());
+}
+
+class FlutterSoundSuiteApp extends StatefulWidget {
+  @override
+  _FlutterSoundSuiteAppState createState() => _FlutterSoundSuiteAppState();
+}
+
+class _FlutterSoundSuiteAppState extends State<FlutterSoundSuiteApp> {
+  List<Map> data = List();
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter sound suite Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter sound suite Demo'),
+        ),
+        body: ChangeNotifierProvider(
+          create: (context) => SoundPlayerModel(),
+          child: Container(
+            child: ListView.builder(
+              itemBuilder: (BuildContext context, int index) {
+                Map item = data[index];
+                return Card(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(item['fileName']),
+                      Text('duration ${item['duration']}'),
+                      Card(
+                        color: Colors.red.shade50,
+                        child: SoundPlayerWidget(
+                          filePath: item['filePath'],
+                          fileName: item['fileName'],
+                          duration: item['duration'],
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+              itemCount: data.length,
+            ),
+          ),
+        ),
+        bottomNavigationBar: SafeArea(
+          child: Container(
+            height: 70,
+            child: SoundRecorderWidget(recordComplete: (String filePath, String fileName, double duration) {
+              setState(() {
+                data.add({
+                  'filePath': filePath,
+                  'fileName': fileName,
+                  'duration': duration,
+                });
+                debugPrint('save to $filePath duration $duration');
+              });
+            }),
+          ),
+        ),
+      ),
+    );
+  }
+}
